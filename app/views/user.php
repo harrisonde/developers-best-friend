1f132c48d1e148f070f7b935a46a89585ede6040
Harrison DeStefano <harrison.destefano@gmail.com>
2014-07-09 20:34:57 -0400

    views

---
+++ app/views/user.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>The Developer's Best Friend</title>
+	</head>
+	<body>
+		<h1>So, you need a few users do ya?</h1>
+		<h2>Well, I got your back on this one!</h2>
+		<p>This application is a handy little tool to aid in the generation of users. Please let me know the number of users you'd like and I'll do the rest.</p>
+		<form method="post" action="/text">
+			<label for="user">Number of lines to generate</label>
+			<input name="user"></input>
+		</form>
+	</body>
+</html>
